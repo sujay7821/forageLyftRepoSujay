cb4a0c2db878812c915e66c7e725fe6aad2ba04f
sujay7821 <144977548+sujay7821@users.noreply.github.com>
2023-09-13 23:35:21 -0400

    Add files via upload

---
+++ car.py
@@ -0,0 +1,10 @@
+from serviceable import serviceable
+
+
+class Car(serviceable):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
+    def needs_service(self):
+        return self.engine.needs_service() or self.battery.needs_service()
---
+++ nubbin_battery.py
@@ -0,0 +1,18 @@
+from abc import ABC
+from Battery import Battery
+from datetime import datetime
+
+
+class NubbinBattery(Battery, ABC):
+    def __init__(self, last_service_date, current_mileage, last_service_mileage, warning_light_is_on):
+        super().__init__(last_service_date)
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+        self.warning_light_is_on = warning_light_is_on
+
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date() or self.need_service():
+            return True
+        else:
+            return False
